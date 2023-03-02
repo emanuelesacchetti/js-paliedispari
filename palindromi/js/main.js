14f00f5d8f3d6023d6dd2c0c6382f467fc844025
emanuele sacchetti <manusacco92@gmail.com>
2023-03-02 23:51:14 +0100

    codice revisonato e dimezzato (ancora con la stassa logica)

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -1,5 +1,5 @@
 let parolaUtente = prompt('inserisci una parola palindroma');
-
+let mezzaParola;
 
 let risultato = trovaPalindromo(parolaUtente);
 
@@ -11,54 +11,27 @@ function trovaPalindromo (parolaInserita) {
     let parolaSenzaSpazi = parolaInserita.split(" ").join("");      //stringa
     let lunghezzaParolaSenzaSpazi = parolaSenzaSpazi.length;      //numero
     let valoreDecrescente = lunghezzaParolaSenzaSpazi - 1;
-
-    if (lunghezzaParolaSenzaSpazi % 2 == 0){            //SE PARI
-
-        mezzaParola = lunghezzaParolaSenzaSpazi / 2;
-        let element = true
-        console.log(mezzaParola);
-
-            for ( let i = 0; i < mezzaParola; i++){
-                
-                if (parolaSenzaSpazi[i] != parolaSenzaSpazi[valoreDecrescente]){
-                    element = false
-
-                    break
-                }else {
-                    valoreDecrescente--
-                }
-
-            }
-            if(element === true){
-                console.log('è palindromo');
-            } else if (element === false) {
-                console.log('non è palindromo');
-            }
     
-    } else {                                   //DISPARI
-       
-        mezzaParola = (lunghezzaParolaSenzaSpazi - 1) / 2;     //escludiamo la lettera centrale e abbiamo due parti uguali
-        let element = true
-        console.log(mezzaParola);
-
-            for ( let i = 0; i < mezzaParola; i++){
-                
-                if (parolaSenzaSpazi[i] != parolaSenzaSpazi[valoreDecrescente]){
-                    element = false
-
-                    break
-                }else {
-                    valoreDecrescente--
-                }
-
-            }
-            if(element === true){
-                console.log('è palindromo');
-            } else if (element === false) {
-                console.log('non è palindromo');
-            }
+    if (lunghezzaParolaSenzaSpazi % 2 == 0){
+        mezzaParola = lunghezzaParolaSenzaSpazi / 2;
+    } else {
+        mezzaParola = (lunghezzaParolaSenzaSpazi - 1) / 2;
     }
 
-
-
+    let element = true
+    for ( let i = 0; i < mezzaParola; i++){
+                
+        if (parolaSenzaSpazi[i] != parolaSenzaSpazi[valoreDecrescente]){
+            element = false
+            break
+        }else {
+            valoreDecrescente--
+        }
+    }
+    
+    if(element === true){
+        console.log('è palindromo');
+    } else if (element === false) {
+        console.log('non è palindromo');
+    }
 }
