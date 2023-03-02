ae4ab57afb0a87b8212f37ef8449fdb0111c10e1
emanuele sacchetti <manusacco92@gmail.com>
2023-03-02 16:18:56 +0100

    aggiornamenti palindromo

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -1,25 +1,32 @@
 parolaUtente = prompt('inserisci una parola palindroma');
 
-lunghezzaParola = parolaUtente.length;
 
-console.log(lunghezzaParola);
 
-parolaSenzaSpazi = parolaUtente.split(" ").join("").length;
 
-console.log(lunghezzaParolaSenzaSpazi);
+let valoreCrescente = 0;
+let valoreDecrescente = lunghezzaParolaSenzaSpazi - 1;
 
-let primaParte = 0;
-let ultimaParte = parolaSenzaSpazi - 1;
 
-if (parolaSenzaSpazi % 2 == 0){            //SE PARI
-
-} else {                                    //DISPARI
-    mezzaParola = (parolaSenzaSpazi - 1) / 2     //escludiamo la lettera centrale
-
-    console.log(mezzaParola);
-
-    for ( let i = 0; i <= mezzaParola; i++){
+function trovaPalindromo (parolaInserita) {
+    if (parolaSenzaSpazi % 2 == 0){            //SE PARI
+    
+    } else {                                   //DISPARI
         
-        if 
-    }
-}
+        parolaSenzaSpazi = parolaInserita.split(" ").join("");      //stringa
+        lunghezzaParolaSenzaSpazi = parolaSenzaSpazi.length;      //numero
+        mezzaParola = (lunghezzaParolaSenzaSpazi - 1) / 2     //escludiamo la lettera centrale e abbiamo due parti uguali
+
+        console.log(mezzaParola);
+
+            for ( let i = 0; i < mezzaParola; i++){
+                
+                if (parolaSenzaSpazi[valoreCrescente] == parolaSenzaSpazi[valoreDecrescente]){
+                    console.log('ok');
+                    valoreCrescente++
+                    valoreDecrescente--
+                }
+            }
+        }
+} 
+
+
