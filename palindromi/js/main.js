3cfb08b5d7417c5144df1ab05c3351b0365f68c6
emanuele sacchetti <manusacco92@gmail.com>
2023-03-02 17:26:36 +0100

    programma palindromi terminato

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -10,14 +10,11 @@ function trovaPalindromo (parolaInserita) {
 
     let parolaSenzaSpazi = parolaInserita.split(" ").join("");      //stringa
     let lunghezzaParolaSenzaSpazi = parolaSenzaSpazi.length;      //numero
-    let valoreCrescente = 0;
     let valoreDecrescente = lunghezzaParolaSenzaSpazi - 1;
 
     if (lunghezzaParolaSenzaSpazi % 2 == 0){            //SE PARI
-    
-    } else {                                   //DISPARI
-       
-        mezzaParola = (lunghezzaParolaSenzaSpazi - 1) / 2     //escludiamo la lettera centrale e abbiamo due parti uguali
+
+        mezzaParola = lunghezzaParolaSenzaSpazi / 2;
         let element = true
         console.log(mezzaParola);
 
@@ -37,8 +34,31 @@ function trovaPalindromo (parolaInserita) {
             } else if (element === false) {
                 console.log('non è palindromo');
             }
-        }
+    
+    } else {                                   //DISPARI
+       
+        mezzaParola = (lunghezzaParolaSenzaSpazi - 1) / 2;     //escludiamo la lettera centrale e abbiamo due parti uguali
+        let element = true
+        console.log(mezzaParola);
 
-} 
+            for ( let i = 0; i < mezzaParola; i++){
+                
+                if (parolaSenzaSpazi[i] != parolaSenzaSpazi[valoreDecrescente]){
+                    element = false
+
+                    break
+                }else {
+                    valoreDecrescente--
+                }
+
+            }
+            if(element === true){
+                console.log('è palindromo');
+            } else if (element === false) {
+                console.log('non è palindromo');
+            }
+    }
 
 
+
+}
