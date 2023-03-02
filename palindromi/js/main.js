cbf256c5a9ea047c7ac47241b38559598934cb7b
emanuele sacchetti <manusacco92@gmail.com>
2023-03-02 16:20:40 +0100

    aggiornamento palindromo 2

--- palindromi/js/main.js
+++ palindromi/js/main.js
@@ -8,12 +8,14 @@ let valoreDecrescente = lunghezzaParolaSenzaSpazi - 1;
 
 
 function trovaPalindromo (parolaInserita) {
-    if (parolaSenzaSpazi % 2 == 0){            //SE PARI
+
+    parolaSenzaSpazi = parolaInserita.split(" ").join("");      //stringa
+    lunghezzaParolaSenzaSpazi = parolaSenzaSpazi.length;      //numero
+
+    if (lunghezzaParolaSenzaSpazi % 2 == 0){            //SE PARI
     
     } else {                                   //DISPARI
-        
-        parolaSenzaSpazi = parolaInserita.split(" ").join("");      //stringa
-        lunghezzaParolaSenzaSpazi = parolaSenzaSpazi.length;      //numero
+       
         mezzaParola = (lunghezzaParolaSenzaSpazi - 1) / 2     //escludiamo la lettera centrale e abbiamo due parti uguali
 
         console.log(mezzaParola);
